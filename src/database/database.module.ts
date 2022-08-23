a4db8c3b51c8274af4c75b20500ce8113d043170
Sneyder <sneyder.h.rodriguez@gmail.com>
2022-08-23 10:34:58 -0500

    instalacion de mongoose y configurado de manera global para los Schema :wavy_dash:

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,5 +1,6 @@
 import { Module, Global } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { MongoClient } from 'mongodb';
 
 import config from '../config';
@@ -9,6 +10,21 @@ const API_KEY_PROD = 'PROD12312';
 
 @Global()
 @Module({
+  imports: [
+    // MongooseModule.forRoot('mongodb://localhost:27017', {
+    //   dbName: 'ecommerce',
+    // }),
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { connection, host, port, dbName } = configService.mongo;
+        return {
+          uri: `${connection}://${host}:${port}`,
+          dbName,
+        };
+      },
+      inject: [config.KEY],
+    }),
+  ],
   providers: [
     {
       provide: 'API_KEY',
@@ -27,6 +43,6 @@ const API_KEY_PROD = 'PROD12312';
       inject: [config.KEY],
     },
   ],
-  exports: ['API_KEY', 'MONGO'],
+  exports: ['API_KEY', 'MONGO', MongooseModule],
 })
 export class DatabaseModule {}
